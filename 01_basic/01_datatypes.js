b5ed29fb608b1fcc5290e4870e6d618a776d4085
Aniket Thakur <aniketthakur599@gmail.com>
2025-11-09 07:34:17 +0000

    datatypes

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";// treat all js code as newer verison
+
+// alert(3+3) // we are using node.js,not browser
+
+console.log(3
+    +3)// code readbility should be high 
+
+console.log("Aniket")
+
+
+
+
+let name ="aniket"
+let age=20
+let isloggedin =false
+
+
+// NOTES
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value/ repesent empty value
+// undefined =>
+// symbol => unique 
+// object 
+
+console.log(typeof null) //object type
+console.log(typeof undefined)// undefined
---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId =144553
+let accountEmail ="aniketthakur599@gmail.com"
+var accountPassword ="12345"
+accountCity = "Himachal Pardesh"
+let accountState;
+// accountId = 2 not allowed
+accountEmail= "ani12@gmail.com"
+accountPassword="123414"
+accountCity ="PUNE"
+console.log(accountId);
+/*
+Prefer not to use var
+because  of issue in block scope and functional scop
+
+
+*/
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
